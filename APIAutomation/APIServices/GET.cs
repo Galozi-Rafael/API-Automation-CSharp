7b3ba6e91e370b7b36ef43b61464c39c6d31a15f
Galozi-Rafael <rafael.hist@yahoo.com.br>
2026-01-29 21:24:36 -0300

    feat: Cria a primeira instância do Projeto de automação de API e um método GET básico.

---
+++ APIAutomation/APIServices/GET.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace APIAutomation.APIServices
+{
+    // Define a classe GET para realizar requisições GET
+    // Esse método pode ser utilizado para testar endpoints de APIs RESTful
+    // e obter dados de servidores web
+    internal class GET
+    {
+        public async Task<string> GetResponseAsync(string url)
+        {
+            // Cria uma instância de HttpClient
+            using (HttpClient client = new HttpClient())
+            {
+                // Envia a requisição GET e obtém a resposta
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                int statusCode = (int)response.StatusCode;
+                
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                return "O código de status é: " + statusCode + "\n\n" + responseBody;
+                
+            }
+        }
+    }
+}
---
+++ APIAutomation/Program.cs
@@ -0,0 +1,22 @@
+﻿using APIAutomation.APIServices;
+using System;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Cria uma instância da classe GET
+        GET apiGET = new GET();
+
+        string url = "https://automationexercise.com/api/productsList";
+
+        // Chama o método GetResponseAsync para obter a resposta da URL fornecida
+        string response = await apiGET.GetResponseAsync(url);
+
+        Console.WriteLine(response);
+    }
+}
+       
